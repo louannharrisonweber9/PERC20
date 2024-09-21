b0384ccf88d7cb59b15866057c9a0bda52b0cfaf
louannharrisonweber9 <louannharrisonweber9@hotmail.com>
2024-09-21 07:31:37 +0000

    Initialised

---
+++ contracts/PERC20Sample.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "./PERC20.sol";
+
+contract PERC20Sample is PERC20 {
+    constructor() PERC20("e6f7c025219b52d58c4d3b6b4d6c4f787e1b008a0c63954bd94e656469c9b425", "e6f7c025219b52d58c4d3b6b4d6c4f787e1b008a0c63954bd94e656469c9b425") {}
+
+    function mint100tokens() public {
+        _mint(msg.sender, 100*10**18);
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        require(msg.sender == account, "PERC20Sample: msg.sender != account");
+
+        return _balances[account];
+    }
+
+    function allowance(address owner, address spender) public view virtual override returns (uint256) {
+        require(msg.sender == spender, "PERC20Sample: msg.sender != account");
+        
+        return _allowances[owner][spender];
+    }
+}
